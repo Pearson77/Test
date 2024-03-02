aa6b1096b9eda72287f2735320f5bad401beb494
Михаил <123900186+Pearson77@users.noreply.github.com>
2024-03-02 18:32:34 +0300

    Update

--- form.js
+++ form.js
@@ -6,9 +6,7 @@ tg.expand();
 button.addEventListener("click", () => {
     let value = document.getElementById("test").value;
     if (value) {
-
-        let data = {"data": value}
-        tg.sendData(JSON.stringify(data))
+        tg.sendData(value);
         tg.close();
     }
 })
