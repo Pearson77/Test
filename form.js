610c224e0a2b1f338b7aef3cdd61791f8a8625dc
Михаил <123900186+Pearson77@users.noreply.github.com>
2024-03-02 18:51:35 +0300

    Update

--- form.js
+++ form.js
@@ -2,7 +2,7 @@ let button = document.getElementById("button");
 let tg = window.Telegram.WebApp;
 
 button.addEventListener("click", () => {
-    let value = document.getElementById("test").value;
+    let value = document.getElementById("inp").value;
     if (value) {
         tg.sendData('test_data');
         tg.close();
